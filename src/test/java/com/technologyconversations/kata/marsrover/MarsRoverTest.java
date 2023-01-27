35ea512dc913359e283fa690daf60234cc3a2d28
Michelle AVOMO <michelle.avomo@codeworks.fr>
2023-01-27 21:08:27 +0100

    production de ce soir

--- src/test/java/com/technologyconversations/kata/marsrover/MarsRoverTest.java
+++ src/test/java/com/technologyconversations/kata/marsrover/MarsRoverTest.java
@@ -3,6 +3,7 @@ package com.technologyconversations.kata.marsrover;
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
 import net.jqwik.api.constraints.Chars;
+import net.jqwik.api.constraints.IntRange;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +18,6 @@ public class MarsRoverTest {
 
         Coordinates roverCoordinates = new Coordinates(x, y, Direction.NORTH, obstacles);
         Rover rover = new Rover(roverCoordinates);
-
         Rover roverBis = new Rover(roverCoordinates);
 
         rover.receiveCommands(command);
@@ -27,6 +27,33 @@ public class MarsRoverTest {
         return roverBis.getCoordinates().equals(rover.getCoordinates()) ;
     }
 
+    @Property
+    boolean shouldVerifyThatItDoesStuff(@ForAll @IntRange(max = 1001) int length) throws Exception {
+        Point x = new Point(1, 9);
+        Point y = new Point(2, 9);
+        List<Obstacle> obstacles = new ArrayList<>();
+
+        Coordinates roverCoordinates = new Coordinates(x, y, Direction.NORTH, obstacles);
+        Rover rover = new Rover(roverCoordinates);
+
+        System.out.println("##my length command::" + length);
+
+        for (int i = 0; i < length; i++) {
+            rover.receiveCommands("F");
+        }
+
+        for (int i = 0; i < length; i++) {
+            rover.receiveCommands("B");
+        }
+
+        return roverCoordinates.equals(rover.getCoordinates()) ;
+
+    }
 
 
-}
+
+
+
+
+
+    }
